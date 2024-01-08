16e2c60846990ed5a403fd0795c74b1a148378ae
ai-tanzil811 <atanzil223028@bscse.uiu.ac.bd>
2024-01-08 07:36:59 +0600

    irst version

---
+++ tanzil.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int x;
+scanf("%d",&x);
+ for(int i=0;i<=x;i++)
+{
+ for (int j=1;j<=i;j++)
+{
+printf("*");
+}
+printf(" \n");
+}
+ for(int i=0;i<=x;i++)
+{
+ for (int j=i;j>=1;j--)
+{
+printf("*");
+}
+printf(" \n");
+}
+return 0;
+}
